2494475e77acaa54cfdd389f5acea5ecf4ed5c9d
Yukhosmith <yuskhosmith1@gmail.com>
2022-10-11 11:35:43 +0100

    heroku

--- QuizConfig/settings.py
+++ QuizConfig/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    "whitenoise.runserver_nostatic",
     'QuizApp',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -43,7 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
